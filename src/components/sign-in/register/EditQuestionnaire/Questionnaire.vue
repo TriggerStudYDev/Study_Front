9e1f5281b447b2e7ca51a9342e9adba1e0f7d54e
Oleg <109240899+Volmol16@users.noreply.github.com>
2025-03-10 22:02:46 +0300

    add-butonQuestionnaire

--- src/components/sign-in/register/EditQuestionnaire/Questionnaire.vue
+++ src/components/sign-in/register/EditQuestionnaire/Questionnaire.vue
@@ -1,9 +1,12 @@
 <template>
-    <div>
+    <div class="flex flex-col items-center">
         <div class="grid grid-cols-3 gap-x-[30px] mt-6">
             <QuestionnaireCard v-for="(card, index) in card" :key="card.id" :card="card"
                 :class="`animate-down delay-${index}`" @open-modal="openModal" />
         </div>
+        <button
+            class="col-span-3 px-8 py-3 text-xl font-medium leading-7 text-white rounded-lg bg-AccentViolet mt-[30%]">Отправить
+            на проверку</button>
     </div>
 </template>
 
